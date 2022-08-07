708b07aa88b9092c8f928ed454cb77f3c505211d
ronaldgulayan <99184659+ronaldgulayan@users.noreply.github.com>
2022-08-06 18:14:04 -0800

    Add files via upload

---
+++ project_js.js
@@ -0,0 +1,60 @@
+
+import { CreateProject, CreateLowerContainer, CreateVideoProject } from "./import.js";
+
+const menuButton = document.querySelector(".menu-button");
+const menuDrawer = document.querySelector(".upper-drawer");
+let drawerIsOpen = false;
+
+menuButton.addEventListener("click",()=>{
+    if(!drawerIsOpen){
+        menuDrawer.classList.add("open");
+        menuButton.classList.add("open");
+        drawerIsOpen = true;
+    }else{
+        menuButton.classList.remove("open");
+        menuDrawer.classList.remove("open");
+        drawerIsOpen = false;
+    }
+})
+
+// MAX WEBSITE
+new CreateProject("#content","https://bit.ly/3Os5uQh", "#bbb", "project-img/max.jpg", ["html", "css", "javascript"],"Max Website","This is our dog, he was born to be an assassin");
+
+// MY FIRST PORTFOLIO
+new CreateProject("#content","https://ronaldgulayan.github.io/my-website-main.com/Ronald.html", "#bbb", "project-img/img-2.jpeg", ["html", "css", "javascript"],"My First Portfolio","This is the very first portfolio I made, I made it when i was just a beginner in coding.")
+
+// MY SECOND PORTFOLIO
+new CreateProject("#content","https://ronaldgulayan.github.io/ron.github.io/main.html", "#bbb", "project-img/img-1.jpeg", ["html", "css", "javascript"],"My Second Portfolio","This is my second Portfolio");
+
+// MY FIRST STUDENT FORM
+new CreateProject("#content","https://ronaldgulayan.github.io/student_form.com/Ronald.html", "#bbb", "project-img/studentform.png", ["html", "css"],"My First Student Form","This is my first Student form");
+
+// CHATTING APPLICATION
+new CreateProject("#content","https://www.mediafire.com/file/p8q4mxmsjryfeep/chatapp-0.1-arm64-v8a_armeabi-v7a-debug.apk/file", "#bbb", "project-img/chatapp.png", ["Python","Kivy","Kivymd"],"Chatapp Application","Here are the chat apps I made, you can chat here but only at close distance.");
+
+// PORTFOLIO APPLICATION
+new CreateProject("#content","https://www.mediafire.com/file/hs7hleonj8ymv4q/exampleApp.apk/file", "#bbb", "project-img/img-3.jpg", ["Java"],"Code Art apps","This is my portfolio application");
+
+// MY APPLICATION
+new CreateProject("#content","https://www.mediafire.com/file/deu67px757jcl79/Ron_application.apk/file", "#bbb", "project-img/person.gif", ["Python", "Kivy", "Kivymd"],"Ron Application","This is my application, It has many tools that will help you hehe");
+
+// VIDEO 1
+new CreateVideoProject("#content", "vid/shop-system.mp4", "#bbb", "img/ordering-img.png", ["Java", "MySQL"], "Shop Ordering System", "This is my first shop ordering system")
+
+// VIDEO 2
+new CreateVideoProject("#content", "vid/school-management-system.mp4", "#bbb", "img/school-system.png", ["Java", "MySQL"], "School Management System", "This is my School management System")
+
+//CONTAINER
+const container = new CreateLowerContainer("lower-container","#fff", "50px");
+CreateLowerContainer.addStyle("#lower-container").boxShadow = "0px -1px 5px rgb(0,0,0,.2)"
+
+//ELEMENT
+container.addText("text1", "RonaldGulayan", "20px","#000", "sans-serif", "white", "0px 2px 5px rgb(0,0,0,.5)")
+CreateLowerContainer.addStyle(".text1").marginTop = "10px";
+container.addText("text2", "ronaldgulayan00@gmail.com", "16px","#000", "sans-serif", "white", "0px 2px 5px rgb(0,0,0,.2)")
+CreateLowerContainer.addStyle(".text2").fontStyle = "italic";
+
+//BUTTONS
+container.CreateButtons()
+CreateLowerContainer.addButtonsEvent();
+
